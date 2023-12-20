83143624bb672586cd5291b651a1f7fb3badd4bf
bilalzrd <126500167+bilalzrd@users.noreply.github.com>
2023-12-20 18:14:37 +0100

    authenticatie branch updated

--- __tests__/users.spec.js
+++ __tests__/users.spec.js
@@ -1,72 +1,93 @@
-const supertest = require('supertest');
-const createServer = require('../app/src/createServer');
 const { getKnex, tables } = require('../app/src/data');
+const supertest = require('supertest');
+const Role = require('../app/src/core/roles');
+const { withServer, login, loginAdmin } = require('./supertest.setup');
+const { testAuthHeader } = require('./common/auth');
 
-describe('User API', () => {
-  let server;
-  let request;
-  let knex;
+
+
+describe('Users', () => {
+
+  let request, knex, authHeader, adminAuthHeader;
+
+  withServer(({
+    supertest,
+    knex: k,
+  }) => {
+    request = supertest;
+    knex = k;
+  });
+
+  const data = [
+    {
+      id:1,
+      username: 'Test User',
+      email: 'test.user@hogent.be',
+      password_hash:
+        '$argon2id$v=19$m=2048,t=2,p=1$NF6PFLTgSYpDSex0iFeFQQ$Rz5ouoM9q3EH40hrq67BC3Ajsu/ohaHnkKBLunELLzU',
+      roles: JSON.stringify([Role.USER]),
+    },
+    {
+      id:2,
+      username: 'Admin User',
+      email: 'admin.user@hogent.be',
+      password_hash:
+        '$argon2id$v=19$m=2048,t=2,p=1$NF6PFLTgSYpDSex0iFeFQQ$Rz5ouoM9q3EH40hrq67BC3Ajsu/ohaHnkKBLunELLzU',
+      roles: JSON.stringify([Role.ADMIN, Role.USER]),
+    },
+  ]
 
   beforeAll(async () => {
-    server = await createServer();
-    request = supertest(server.getApp().callback());
-    knex = getKnex();
-  });
-
-  afterAll(async () => {
-    await server.stop();
-  });
+    // Insert test data into the 'users' table before tests start
+  })
 
   beforeEach(async () => {
-    // Clear the dependent table (journal) before each test
-    await knex('journal').delete();
-
     // Clear the users table
     await knex('users').delete();
   });
 
-  it('should get all users', async () => {
-    await knex(tables.users).insert([
-      { id:1 , username: 'User1', email: 'user1@example.com', password: 'password1' },
-      { id: 2, username: 'User2', email: 'user2@example.com', password: 'password2' },
-    ]);
 
-    const response = await request.get('/api/nexus/allUsers');
+
+  it('should get all users', async () => {
+
+    await knex(tables.users).insert(data);
+
+    const response = await request.get('/api/user/users');
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveLength(2);
     // Add more expectations based on your user data and response structure
   });
-
-  it('should get a user by ID', async () => {
+  
+  it('should get user by ID', async () => {
+    await knex(tables.users).insert(data[0])
     const userId = 1;
-      await knex(tables.users).insert({id: userId, username: 'User1', email: 'user1@example.com', password: 'password1' });
-
-    const response = await request.get(`/api/nexus/user/${userId}`);
-
+    const response = await request.get(`/api/user/${userId}`);
     expect(response.status).toBe(200);
     expect(response.body.id).toBe(userId);
-    // Add more expectations based on your user data and response structure
   });
 
-  it('should add a new user', async () => {
-    const newUser = {
-      username: 'NewUser',
-      email: 'newuser@example.com',
-      password: 'newpassword',
-    };
-  
-    const response = await request.post('/api/nexus/addUser').send(newUser);
-  
+  it('should get user count', async () => {
+    await knex(tables.users).insert(data)
+    const response = await request.get('/api/user/count');
     expect(response.status).toBe(200);
-    expect(response.body.success).toBe(true);
-  
-    // Add a delay to allow the database operation to complete
-    
-    // Check if the user was added to the database using the username
-    const addedUser = await knex(tables.users).where('username', newUser.username).first();
-    expect(addedUser).toBeDefined();
-    expect(addedUser.username).toBe(newUser.username);
-    // Add more expectations based on your user data and response structure
-  });  
+  });
+
+  it('should get user by email', async () => {
+    await knex(tables.users).insert(data)
+    const userEmail = 'test.user@hogent.be';
+    const response = await request.get(`/api/user/email/${userEmail}`);
+    expect(response.status).toBe(200);
+    expect(response.body.email).toBe(userEmail);
+  });
+
+  it('should get user by username', async () => {
+    await knex(tables.users).insert(data)
+    const username = 'Test User';
+    const response = await request.get(`/api/user/username/${username}`);
+    expect(response.status).toBe(200);
+    expect(response.body.username).toBe(username);
+  });
+
+  // Add more test cases for other endpoints (register, login, delete, etc.)
 });
--- app/src/core/serviceError.js
+++ app/src/core/serviceError.js
@@ -1,6 +1,9 @@
 const NOT_FOUND = 'NOT_FOUND';
 const VALIDATION_FAILED = 'VALIDATION_FAILED';
 
+const UNAUTHORIZED = 'UNAUTHORIZED'; 
+const FORBIDDEN = 'FORBIDDEN';
+
 class ServiceError extends Error {
 
   constructor(code, message, details = {}) {
@@ -24,6 +27,22 @@ class ServiceError extends Error {
 
   get isValidationFailed() {
     return this.code === VALIDATION_FAILED;
+  }
+
+  //
+
+  static unauthorized(message, details) {
+    return new ServiceError(UNAUTHORIZED, message, details);
+  }
+
+  static forbidden(message, details) {
+    return new ServiceError(FORBIDDEN, message, details);
+  }
+  get isUnauthorized() {
+    return this.code === UNAUTHORIZED;
+  }
+  get isForbidden() {
+    return this.code === FORBIDDEN;
   }
 }
 
